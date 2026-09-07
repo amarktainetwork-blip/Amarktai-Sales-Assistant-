8cc8db61131fe2005962031593c1ba7adec5abe1
amarktainetwork-blip <amarktainetwork@gmail.com>
2026-09-07 08:55:35 +0200

    Load final client-facing handover polish after dashboard styles

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -11,6 +11,7 @@ import ErrorBoundary from "./components/ErrorBoundary";
 import { DashboardLayoutSkeleton } from "./components/DashboardLayoutSkeleton";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import "./dashboard-final.css";
+import "./handover-final.css";
 import AboutPage from "./marketing/AboutPage";
 import ContactPage from "./marketing/ContactPage";
 import {
